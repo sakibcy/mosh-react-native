acb4eaa1a247e30d7487f92f65d76a3c94bcd687
Md Sakibul Hasan <sakibal623@gmail.com>
2022-06-19 01:46:18 +0600

    Builded ListingsScreen.

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { Platform, SafeAreaView, StyleSheet, View } from "react-native";
 import Constants from "expo-constants";
 
 export default function Screen({ children, style }) {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return (
+    <SafeAreaView style={[styles.screen, style]}>
+      {/*  TODO: Test on iOS to see if the padding work without View */}
+      <View style={Platform.OS === "ios" && style}>{children}</View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
