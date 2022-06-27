1295e674317a37ccb6bc73715e7e37c1858e220a
Md Sakibul Hasan <sakibal623@gmail.com>
2022-06-27 19:08:26 +0600

    refactor the restoreUser func.

--- App.js
+++ App.js
@@ -37,16 +37,15 @@ export default function App() {
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState(false);
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (!token) return;
-    setUser(jwtDecode(token));
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
   };
 
   if (!isReady)
     return (
       <AppLoading
-        startAsync={restoreToken}
+        startAsync={restoreUser}
         onFinish={() => setIsReady(true)}
         onError={() =>
           console.log("There is an error while loading the screen ")
--- app/auth/storage.js
+++ app/auth/storage.js
@@ -1,4 +1,5 @@
 import * as SecureStorage from "expo-secure-store";
+import { jwtDecode } from "jwt-decode";
 
 const key = "authToken";
 
@@ -8,6 +9,11 @@ const storeToken = async (authToken) => {
   } catch (error) {
     console.log("Error while storing token ", error);
   }
+};
+
+const getUser = async () => {
+  const token = await getToken();
+  return token ? jwtDecode(token) : null;
 };
 
 const getToken = async () => {
@@ -26,4 +32,4 @@ const removeToken = async () => {
   }
 };
 
-export default { storeToken, getToken, removeToken };
+export default { storeToken, getUser, removeToken };
